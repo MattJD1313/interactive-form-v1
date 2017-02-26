a01ee9ee61496932ee29a020bc4c61d81c469877
MattJD1313 <foli2012@gmail.com>
2017-02-26 13:00:07 -0700

    More Optimizations
    Moved all script to main.js and removed the other scripts from the
    index.html file.

--- js/main.js
+++ js/main.js
@@ -1,28 +1,54 @@
 
-//Assigning variables.
+/*
+*************************************************
+ASSIGNING FORM VARIABLES
+*************************************************
+*/
+
+//BASIC INFO VARIABLES
 const nameField = document.getElementById('name');
 const mailField = document.getElementById('mail');
 const jobTitle = document.getElementById('title');
+//T-SHIRT VARIABLES
 const tShirtDesign = document.getElementById('design');
 const tShirtColor = document.getElementById('color');
+//ACTIVITIES CHECKBOX VARIABLES
 const registerForActivities = document.getElementsByClassName('activities')[0];
 let checkBoxInfo = [];
 let workShopInfo = [];
 let total = 0;
+//PAYMENT INFO VARIABLES
+const today = new Date().toISOString().slice(0,10).split('-');
+const paymentMethod = document.getElementById('payment');
+const creditCard = document.getElementById('credit-card');
+const creditCardNum = document.getElementById('cc-num');
+const creditCardZip = document.getElementById('zip');
+const creditCardCvv = document.getElementById('cvv');
+const creditCardExpMonth = document.getElementById('exp-month');
+const creditCardExpYear = document.getElementById('exp-year');
+const payPal = creditCard.nextElementSibling;
+const bitCoin = creditCard.nextElementSibling.nextElementSibling;
+const paymentTypes = [[creditCard,'credit card'],[payPal,'paypal'],[bitCoin,'bitcoin']];
 
+/*
+ *************************************************
+ ASSIGNING EVENT LISTENERS
+ *************************************************
+*/
 
-//Adding EventListeners for different selection fields.
+//BASIC INFO LISTENERS
 jobTitle.addEventListener('change',otherTextArea);
-
+mailField.addEventListener('keypress',validateEmail);
+//T-SHIRT LISTENERS
 tShirtDesign.addEventListener('change',tShirtDesignNarrowed);
-
+//ACTIVITIES CHECKBOX VARIABLES
 registerForActivities.addEventListener('change', function (){
     checkBoxInfoPush();
     totalDollars();
     hideTotalDollars();
     getCheckedIndexInfo();
 });
-
+//PAYMENT INFO LISTENERS
 paymentMethod.addEventListener('change', function () {
     hidePaymentElements();
     if(paymentMethod.value == 'select_method'){
@@ -32,20 +58,11 @@ paymentMethod.addEventListener('change', function () {
     }
 });
 
-mailField.addEventListener('keypress',validateEmail);
-
-//Initializing parameters.
-let initialize = function() {
-    initialize = function(){};
-    nameField.focus();
-    tShirtColor.appendChild(initialColorOption);
-    tShirtDesignNarrowed();
-    resetAndHide(0,5,tShirtColor,6);
-    hideTotalDollars();
-    hidePaymentElements();
-};
-initialize();
-
+/*
+*************************************************
+JOB SELECTION SECTION
+*************************************************
+*/
 
 //Dynamically inserts a textArea when the 'other' option is selected.
 function otherTextArea() {
@@ -59,6 +76,13 @@ function otherTextArea() {
         jobTitle.parentNode.removeChild(otherTextArea);
     }
 }
+
+/*
+*************************************************
+T-SHIRT SELECTION SECTION
+*************************************************
+*/
+
 
 //Creating an initial select option for T-shirt color.
 let initialColorOption = document.createElement('Option');
@@ -95,11 +119,9 @@ function tShirtDesignNarrowed() {
 }
 
 /*
-****************************************
-
-Activities section of the form.
-
-****************************************
+*************************************************
+ACTIVITIES CHECKBOX SECTION
+*************************************************
 */
 
 //Creates an array from the label element's text.
@@ -209,3 +231,177 @@ function totalDollars() {
     let totalText = document.createTextNode(total.toString());
     document.getElementById('activityTotalCost').appendChild(totalText);
 }
+
+/*
+*************************************************
+PAYMENT INFO SECTION
+*************************************************
+*/
+
+
+//Loops through payment types, shows the fields for the types selected, and hides the ones not selected.
+function hidePaymentElements() {
+    for(let i = 0 ; i < paymentTypes.length ; i++){
+        if(paymentMethod.value == paymentTypes[i][1] && paymentTypes[i][0].hasAttribute('hidden')){
+            paymentTypes[i][0].removeAttribute('hidden')
+        } else if(paymentMethod.value != paymentTypes[i][1] && paymentTypes[i][0].hasAttribute('hidden') == false){
+            paymentTypes[i][0].setAttribute('hidden','hidden');
+        }
+    }
+}
+
+/*
+ Checks to see if the credit card option is selected. Validates that the credit
+ number is 16 digits long and triggers an alert if it is not. The expiration date
+ is also validated against today's date and an alert is generated if the date
+ entered has expired. Both of these alerts are triggered by the submit button.
+ */
+function creditCardValidation() {
+    //Checks to see if credit card option is selected.
+    if (creditCard.hasAttribute('hidden') == false) {
+        //Validates that the field is not empty.
+        if (creditCardNum.value == '') {
+            creditCard.children[0].firstElementChild.style.color = 'red';
+            alert('Please enter a valid credit card number');
+            //Validates length of the credit card number.
+        } else if (creditCardNum.value.replace(/\s/g, "").length != 16) {
+            creditCard.children[0].firstElementChild.style.color = 'red';
+            alert('A valid credit card number has 16 digits, the number you have entered has ' + creditCardNum.value.length + ' digits.');
+        } else {
+            creditCard.children[0].firstElementChild.removeAttribute('style');
+        }
+        if (creditCardZip.value.length != 5) {
+            creditCard.children[1].firstElementChild.style.color = 'red';
+        } else {
+            creditCard.children[1].firstElementChild.removeAttribute('style');
+        }
+        if (creditCardCvv.value == '') {
+            creditCard.children[2].firstElementChild.style.color = 'red';
+        } else {
+            creditCard.children[2].firstElementChild.removeAttribute('style');
+        }
+        //Validating the expiration date.
+        if (parseInt(creditCardExpYear.value) < parseInt(today[0])) {
+            creditCard.children[4].previousElementSibling.style.color = 'red';
+            creditCard.children[6].previousElementSibling.style.color = 'red';
+            alert('It seems like your credit card expired in ' + creditCardExpYear.value + ' , please enter a valid expiration date.')
+        } else if (parseInt(creditCardExpYear.value) == parseInt(today[0]) && parseInt(creditCardExpMonth.value) < parseInt(today[1])) {
+            creditCard.children[4].previousElementSibling.style.color = 'red';
+            creditCard.children[6].previousElementSibling.removeAttribute('style');
+            alert('It seems like your credit card expired earlier this year, please enter a valid expiration date.')
+        } else {
+            creditCard.children[4].previousElementSibling.removeAttribute('style');
+            creditCard.children[6].previousElementSibling.removeAttribute('style');
+        }
+    }
+}
+/*
+*************************************************
+FORM VALIDATION
+*************************************************
+*/
+
+/*
+ Cycles through various fields and displays an error message
+ when the field is blank or in the default position when the
+ submit button is clicked. It removes the error display if the
+ fields have been filled out and the submit button is clicked.
+ */
+function fieldValidation() {
+    if(nameField.value == '') {
+        errorMessage(nameField,'Oops... it looks like you forgot to enter your name');
+    }else{
+        removeErrorMessage(nameField);
+    }
+    if(mailField.value == '') {
+        errorMessage(mailField,'Oops... it looks like you forgot to enter your email address');
+    }else{
+        removeErrorMessage(mailField);
+    }
+    if(tShirtDesign.selectedIndex == 0){
+        errorMessage(document.getElementById('size').parentNode,'Oops... it looks like you forgot to select a t-shirt design');
+    }else{
+        removeErrorMessage(document.getElementById('size').parentNode);
+    }
+    if(checkBoxInfo.length == 0){
+        errorMessage(registerForActivities.children[1],'Oops... it looks like you forgot register for any activities');
+    }else{
+        removeErrorMessage(registerForActivities.children[1]);
+    }
+    if(paymentMethod.value == 'select_method'){
+        errorMessage(paymentMethod,'You have not selected a payment method');
+    }else{
+        removeErrorMessage(paymentMethod);
+    }
+}
+
+//Creates an error message span.
+function errorMessage(element,message) {
+    let elementTitle = element.previousElementSibling;
+    if (elementTitle.children.length == 0) {
+        let elementAlertSpan = document.createElement('Span');
+        let elementTitleAlertText = document.createTextNode('(' + message + ')');
+        elementAlertSpan.appendChild(elementTitleAlertText);
+        elementAlertSpan.className = 'error_alert';
+        elementTitle.appendChild(elementAlertSpan);
+    }
+}
+
+//Removes the error message span.
+function removeErrorMessage(element) {
+    let elementTitle = element.previousElementSibling;
+    let errorMessage = elementTitle.children[0];
+    if (elementTitle.children.length != 0) {
+        elementTitle.removeChild(errorMessage);
+    }
+}
+
+//Adds click event to trigger validation.
+function button(){
+    let button = document.getElementsByTagName('button');
+    for (let i = 0; i < button.length ; i++){
+        button[i].addEventListener('click',function(event){
+            event.preventDefault();
+            fieldValidation();
+            creditCardValidation();
+        });
+    }
+ }
+ button();
+
+/*
+Uses a regular expression to validate the email address format and
+formats the field and displays an error message in real time until
+the format is correct.
+*/
+function validateEmail() {
+    removeErrorMessage(mailField);
+    if (mailField.value != '') {
+        let mailFormat = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        if (mailFormat.test(mailField.value)) {
+            mailField.removeAttribute('style');
+            removeErrorMessage(mailField);
+        } else {
+            errorMessage(mailField,'Please enter a valid email address');
+            mailField.style.borderColor = "red";
+        }
+    }
+}
+
+/*
+*************************************************
+INITIALIZING AND SUBMITTING
+*************************************************
+*/
+
+//Initializing parameters.
+let initialize = function() {
+    initialize = function(){};
+    nameField.focus();
+    tShirtColor.appendChild(initialColorOption);
+    tShirtDesignNarrowed();
+    resetAndHide(0,5,tShirtColor,6);
+    hideTotalDollars();
+    hidePaymentElements();
+};
+initialize();
